d9e2a34c47d0d0d4eff30ffe35fc1e450e61b1b7
@Kubat555 <kubat.chyngyzov@gmail.com>
2023-10-20 15:22:54 +0600

    Change front for Shizik!!!

--- api/services/auth.services.js
+++ api/services/auth.services.js
@@ -3,12 +3,13 @@ import { http } from "../http";
 
 class AuthService {
     async login( data ) {
-        const response = await http.post('api/auth/login', data);
+        const response = await http.post('auth/login', data);
         return response.data;
     }
     async register( registerData ) {
-        const response = await http.post('api/auth/register', registerData);
-        return response.data;
+        const response = await http.post('auth/register', registerData);
+        console.log(response.status);
+        return response.status;
     }
     // async logout() {
     //     const response = await http.post('/');
